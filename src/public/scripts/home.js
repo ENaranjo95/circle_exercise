7f0ac01dbae7605777af3b3820fca0a93c96b6d2
Edwin <edwinx.naranjo@gmail.com>
2018-08-20 01:14:34 -0400

    working on new method to append with less html tags

--- src/public/scripts/home.js
+++ src/public/scripts/home.js
@@ -1,6 +1,38 @@
 let submit = document.getElementsByClassName("button")[0]
 
 
+// submit.addEventListener('click', (event) => {
+//   event.preventDefault()
+//   let myNode = document.getElementById('results')
+//   myNode.innerHTML = '';
+//   let text = document.getElementById("text").value
+//   axios.post('/pixelate', {
+//     'text': text
+//   })
+//   .then((response) => {
+//     let pixel = response.data
+//     let count = 0
+//     let pCount = 0
+//     let p = document.createElement('p')
+//     let parent = document.getElementById('results');
+//     for(let i = 0; i < pixel.length; i++){
+//       count++
+//       let text = document.createTextNode(`${pixel[i]} `)
+//       p.appendChild(text)
+//       if(count === 5){
+//         parent.append(p)
+//         count = 0
+//         pCount++;
+//         p = "div" + pCount
+//         p = document.createElement('p')
+//       }
+//     }
+//     console.log(response.data)
+//   }).catch(function (error) {
+//     console.log(error);
+//   });
+// })
+
 submit.addEventListener('click', (event) => {
   event.preventDefault()
   let myNode = document.getElementById('results')
@@ -12,22 +44,19 @@ submit.addEventListener('click', (event) => {
   .then((response) => {
     let pixel = response.data
     let count = 0
-    let divCount = 0
-    let divName = document.createElement('div')
     let div = document.createElement('div')
+    let parent = document.getElementById('results');
     for(let i = 0; i < pixel.length; i++){
       count++
-      let parent = document.getElementById('results');
       let p = document.createElement('p')
       let text = document.createTextNode(pixel[i])
       p.append(text)
-      divName.append(p)
+      div.append(p)
       if(count === 5){
-        parent.append(divName)
+        parent.append(div)
         count = 0
-        divCount++;
-        divName = "div"+divCount
-        divName = document.createElement('div')
+        div = "div" + i
+        div = document.createElement('div')
       }
     }
     console.log(response.data)

02a5808b4d77a3ec0aed75441615a187e60a2f76
Edwin <edwinx.naranjo@gmail.com>
2018-08-25 22:10:40 -0400

    update

--- src/services/text-valid.js
+++ src/services/text-valid.js
@@ -1,11 +1,12 @@
 const pixel = require('./text-service.js')
+const error = new Error('Words Must Only Include Letters!')
 
 // Checks for the validity of the word in test
 exports.testRun = (text) => {
   let invalid = "`~!@#$%^&*()-_=+{[]}|'\';:,./<>?1234567890"
   for(let i = 0; i < invalid.length; i++){
     if( text.includes(invalid[i]) ){
-      throw 'Words Must Only Include Letters!'
+      throw error
     }
   }
   return text
